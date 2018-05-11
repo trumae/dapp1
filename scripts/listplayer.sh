a10c46cf2327bfc238b81739e2c2214cc433b36f
trumae <trumae@gmail.com>
2018-05-11 03:08:41 -0300

    addUser ok

--- scripts/listplayer.sh
+++ scripts/listplayer.sh
@@ -1,1 +1,1 @@
-cleos -u http://127.0.0.1:8888/ get table game1 game1 player
+cleos -u http://192.168.0.111:8888/ --wallet-url http://192.168.0.111:8899  get table game1 game1 player
--- scripts/newplayer.sh
+++ scripts/newplayer.sh
@@ -3,4 +3,4 @@ read login
 echo "password:"
 read password 
 
-cleos -u http://127.0.0.1:8888/ push action game1 newplayer "[\"$login\",\"$password\"]" -p game1
+cleos  -u http://192.168.0.111:8888/ --wallet-url http://192.168.0.111:8899  push action game1 newplayer "[\"$login\",\"$password\"]" -p game1
